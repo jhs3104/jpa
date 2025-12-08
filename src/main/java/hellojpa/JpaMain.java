c9ff98b1633a250a3c399e191ca0281406cc99ba
jhs3104 <jhs3104@naver.com>
2025-12-08 22:47:38 +0900

    영속성 컨텍스트 영속엔티티 동일성 보장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,6 +19,8 @@ public class JpaMain {
             Member findMember1 = em.find(Member.class, 101L);
             Member findMember2 = em.find(Member.class, 101L);
 
+            System.out.println("(findMember2 == findMember1) = " + (findMember2 == findMember1));
+
             tx.commit();
 
         } catch (Exception e) {
