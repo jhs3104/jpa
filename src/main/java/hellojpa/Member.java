bb79f07db81aded765bccc4c33af9bc760d9e215
jhs3104 <jhs3104@naver.com>
2025-12-15 23:31:10 +0900

    DDL 생성 기능 - 유니크 제약

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -9,8 +10,9 @@ public class Member {
 
     @Id
     private Long id;
+
+    @Column(unique = true, length = 10)
     private String name;
-    private int ggg;
 
     public Member() {
     }
